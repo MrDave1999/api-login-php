28bf5225def8c099ceb346ea341a993568feed9b
MrDave1999 <daveseva2010@hotmail.es>
2021-09-19 12:40:39 -0500

    An access modifier has been changed

--- app/Http/Repositories/BaseRepository.php
+++ app/Http/Repositories/BaseRepository.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class BaseRepository implements IBaseRepository
 {
-    public function __construct(private Model $model)
+    public function __construct(protected Model $model)
     {
         
     }
